cca0b960c9fba4086ea39c18cafb0eed32376652
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-05-22 16:15:40 +0200

    Lesson Task 3 finished

---
+++ js/script.js
@@ -0,0 +1,115 @@
+//Question 1: Given these two variables
+//var firstNumber = 34;
+//var secondNumber = 89;
+//write an if statement that checks whether firstNumber is smaller than secondNumber.
+//If it is, log the message "The first number is smaller than the second".
+
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+    console.log("The first number is smaller than the second");
+}
+
+//Question 2: Given these two variables
+//var age = 11;
+//var minimumAge = "13";
+//write code that checks whether age meets the minimumAge requirement.
+//Log a message if it passes the test and use an else block to log a message if it doesn't.
+
+var age = 11;
+var minimumAge = "13";
+
+if (age > minimumAge) {
+    console.log("Welcome, you are old enough");
+} else {
+    console.log("I'm sorry, you're not old enough");
+}
+
+//Question 3: Given these two variables
+//var income = "11.050";
+//var maximumIncome = "13.075";
+//write code that checks whether income meets the maximumIncome requirement.
+//Log a message if it passes the test and use an else block to log a message if it doesn't.
+
+var income = "11.050";
+var maximumIncome = "13.075";
+
+if (income <= maximumIncome) {
+    console.log("You passed the income check!");
+} else {
+    console.log("I'm sorry, you'reincome is too high!");
+}
+
+//Question 4: Fix this code:
+/*
+var colour = "blue";
+if ((colour = "orange")) {
+    console.log("This colour is a bit rubbish");
+}
+*/
+
+var colour = "blue";
+if (colour === "orange") {
+    console.log("This colour is a bit rubbish");
+}
+
+//Question 5: Given the following variable
+//var invoicePaid = false;
+//write code that logs the message "Not paid" if the invoice has not been paid.
+
+var invoicePaid = false;
+
+if (invoicePaid === false) {
+    console.log("Not paid");
+}
+
+//Question 6: Given these two variables
+//var selectedNumber = 11;
+//var winningNumber = "15";
+//write code that checks if the winning number hasn't been selected.
+//If it hasn't, log the message "You lose again". Use the strict inequality operator.
+
+var selectedNumber = 11;
+var winningNumber = "15";
+
+if (selectedNumber !== winningNumber) {
+    console.log("You lose again");
+}
+
+//Question 7: Given this variable
+//var dayOfTheWeek = 2;
+//write a switch statement that log the weekday name depending on the value of dayOfTheWeek.
+//If dayOfTheWeek is 1, log "Monday", if it is 2 log "Tuesday".
+//If the dayOfTheWeek is not a number between 1 and 7, log the message "Invalid day number".
+
+var dayOfTheWeek = 7;
+var day;
+
+switch (dayOfTheWeek) {
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    case 7:
+        day = "Sunday";
+        break;
+    default:
+        day = "Invalid day number";
+}
+
+console.log(day);
